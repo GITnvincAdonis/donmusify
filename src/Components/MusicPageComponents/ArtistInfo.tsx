d95e4d213e45d738f91ddf01c767c67a580c2968
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-10-01 15:43:07 -0400

    front end design

--- src/Components/MusicPageComponents/ArtistInfo.tsx
+++ src/Components/MusicPageComponents/ArtistInfo.tsx
@@ -1,4 +1,62 @@
 import "bootstrap/dist/css/bootstrap.css";
+
 export default function ArtistInfo() {
-  return <></>;
+  const songs = [1, 1, 1, 1, 1];
+  const albums = [1, 1, 1];
+  return (
+    <>
+      <div className="card text-bg-dark my-card-container">
+        <img src="..." className="card-img artist-image" />
+        <div className="card-img-overlay artist-text-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
+      </div>
+
+      <div className="top-songs">
+        <h2>TOP songs</h2>
+
+        {songs.map((item, index) => {
+          console.log(item);
+          return (
+            <div key={index} className={`side-song-info`}>
+              {index + 1}.
+              <img
+                className={`side-song-image side-song-image-${index + 1}`}
+                src="..."
+                alt=""
+              />
+              <div className={`  side-song-info-text`}>
+                <h3 className={`side-song-name`}> Song </h3>
+                <h4 className={`side-song-duration`}>1:20</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="top-songs">
+        <h2>Albums</h2>
+        {albums.map((item, index) => {
+          console.log(item);
+          return (
+            <div key={index} className={`side-song-info`}>
+              {index + 1}.
+              <img
+                className={`side-song-image side-song-image-${index + 1}`}
+                src="..."
+                alt=""
+              />
+              <div className={`  side-song-info-text`}>
+                <h3 className={`side-song-name`}> Song </h3>
+                <h4 className={`side-song-duration`}>1:20</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
--- src/Components/MusicPageComponents/MusicResults.tsx
+++ src/Components/MusicPageComponents/MusicResults.tsx
@@ -1,3 +1,31 @@
 export default function MusicResults() {
-  return <></>;
+  const songs = [1, 1, 1, 1, 1, 1, 1, 1];
+  return (
+    <>
+      <div className="upper-song-container"></div>
+      <div className="retrieved-songs">
+        {songs.map((item, index) => {
+          console.log(item);
+          return (
+            <div key={index} className={`  main-song-info`}>
+              {index + 1}.
+              <img
+                className={`main-song-image main-song-image-${index + 1}`}
+                src="..."
+                alt=""
+              />
+              <div className={`  main-song-info-text`}>
+                <div className={`main-song-name`}>
+                  {" "}
+                  <h3>fetched Song Name</h3>
+                  <div>simba/adsv/ad</div>
+                </div>
+                <h4 className={`main-song-duration`}>1:20</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
